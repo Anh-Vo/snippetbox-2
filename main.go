047a6b792db87cfbee0f1b66c32977df3bbc55f7
Anh Vo <anhvoo.dev@gmail.com>
2022-09-23 21:25:00 -0700

    feat: add id parameter to snippet view endpoint

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -10,11 +12,20 @@ func home(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	}
+	// for k, v := range w.Header() {
+	// 	fmt.Printf("key: %s      value: %s\n", k, v)
+	// }
+	w.Header()["Date"] = nil
 	w.Write([]byte("Hello from Anh Vo"))
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a specific snippet..."))
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
